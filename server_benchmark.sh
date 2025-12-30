ba35dca2cc9cc0dbf4c4e38fc31273be68c0a33b
kiaisoft <you@example.com>
2025-12-30 08:24:25 +0700

    feat: update concurrency usage

--- server_benchmark.sh
+++ server_benchmark.sh
@@ -33,4 +33,4 @@ unzip -o "${tmp_zip}" -d images
 rm -f "${tmp_zip}"
 
 # Run benchmark.
-bash ./run_all.sh --all-platforms --all-models --max-images 100
+bash ./run_all.sh --all-platforms --all-models --max-images 10 --concurrency 2
