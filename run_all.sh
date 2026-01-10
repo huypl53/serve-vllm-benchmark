d4e981731f5f9b22ada8279b1d879c7ca03dd35b
huypl53 <huypl259@gmail.com>
2026-01-10 19:17:50 +0700

    feat: update VLM set to be used

--- run_all.sh
+++ run_all.sh
@@ -62,8 +62,9 @@ while [[ $# -gt 0 ]]; do
             shift
             ;;
         --all-models)
-            # MODELS="qwen2.5-vl-7b,qwen2.5-vl-7b-w8a8,qwen2.5-vl-3b,qwen2.5-vl-3b-w4a16,qwen3-vl-8b,qwen3-vl-4b,internvl3.5-2b,internvl3.5-4b,internvl3.5-1b,erax-vl-2b,vintern-1b,vistral-7b-chat,pangea-7b,lavy-instruct"
-            MODELS="qwen2.5-vl-7b,qwen2.5-vl-7b-w8a8,qwen2.5-vl-3b,qwen2.5-vl-3b-w4a16,qwen3-vl-8b,qwen3-vl-4b,vistral-7b-chat,pangea-7b,lavy-instruct,qwen2.5-omni-3b,kimi-vl-a3b-thinking"
+            # MODELS="qwen2.5-vl-7b,qwen2.5-vl-7b-w8a8,qwen2.5-vl-3b,qwen2.5-vl-3b-w4a16,qwen3-vl-8b,qwen3-vl-4b,internvl3.5-2b,internvl3.5-4b,internvl3.5-1b,erax-vl-2b,vintern-1b,pangea-7b,lavy-instruct"
+            # pangea-7b: incompatible  model architecture
+            MODELS="qwen2.5-vl-7b,qwen2.5-vl-7b-w8a8,qwen2.5-vl-3b,qwen2.5-vl-3b-w4a16,qwen3-vl-8b,qwen3-vl-4b,lavy-instruct,qwen2.5-omni-3b,kimi-vl-a3b-thinking"
             shift
             ;;
         --merge-outputs)
